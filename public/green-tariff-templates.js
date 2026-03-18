3e5dd80a9e8b83bdf3ead29e980e60a3f692ae2a
andros <andros82@gmail.com>
2026-03-18 17:57:34 +0200

    v5_2

--- public/green-tariff-templates.js
+++ public/green-tariff-templates.js
@@ -155,9 +155,11 @@ const GT_TEMPLATES = {
             .gt-overlay-container {
                 position: relative;
                 width: 100%;
+                min-height: 500px; /* Запобігає зхлопуванню, якщо фото вантажиться повільно */
                 background: #fff;
                 border: 1.5px solid #000;
                 overflow: hidden;
+                margin: 10px 0;
             }
             .gt-overlay-img {
                 display: block;
@@ -166,11 +168,12 @@ const GT_TEMPLATES = {
             }
             .gt-overlay-label {
                 position: absolute;
-                font-size: 8.5pt;
+                font-size: 10pt;
                 line-height: 1.2;
-                color: #000;
+                color: #1a1a1a;
                 font-weight: bold;
                 pointer-events: none;
+                text-shadow: 0 0 2px #fff; /* Покращує читабельність на фоні ліній */
             }
             p { margin: 5px 0; }
         </style>
@@ -761,10 +764,6 @@ const GT_TEMPLATES = {
     doc3: `
         {{styles}}
         <div class="p2-page p2-appendix-page" style="padding: 15mm;">
-            <div style="text-align:right; font-size:11pt; margin-bottom:10px;">
-                <p style="font-weight:bold; margin:0;">«ЗАТВЕРДЖУЮ»</p>
-                <p style="margin:16px 0 0;">_________________ Петро ПАСТУШОК</p>
-            </div>
 
             <p style="font-size:14pt; font-weight:bold; text-align:center; text-transform:uppercase; letter-spacing:0.5px; margin:0 0 10px;">
                 Схема електрична однолінійна
@@ -774,42 +773,42 @@ const GT_TEMPLATES = {
                 <img src="doc/shema_merezeva.jpg" class="gt-overlay-img">
                 
                 <!-- Підстанція -->
-                <div class="gt-overlay-label" style="top: 2%; left: 3%;">
+                <div class="gt-overlay-label" style="top: 5.5%; left: 3%;">
                     «{{field14}}»
                 </div>
                 
                 <!-- Опора -->
-                <div class="gt-overlay-label" style="top: 2%; left: 31%;">
+                <div class="gt-overlay-label" style="top: 5.5%; left: 31%;">
                     Опора №{{field16}}
                 </div>
                 
                 <!-- Лінія -->
-                <div class="gt-overlay-label" style="top: 10%; left: 19%;">
+                <div class="gt-overlay-label" style="top: 10.5%; left: 19%;">
                     {{field15}}
                 </div>
                 
                 <!-- Вхідний автомат -->
-                <div class="gt-overlay-label" style="top: 14%; left: 43%;">
+                <div class="gt-overlay-label" style="top: 15%; left: 43%;">
                     Вхідний автомат<br>
                     Напруга: 0.4 кВ<br>
                     Струм: {{field19}}
                 </div>
                 
                 <!-- Лічильник -->
-                <div class="gt-overlay-label" style="top: 32%; left: 45%;">
+                <div class="gt-overlay-label" style="top: 33.5%; left: 45%;">
                     Двонаправлений лічильник<br>
                     «{{field17}}»
                 </div>
                 
                 <!-- Навантаження -->
-                <div class="gt-overlay-label" style="top: 74%; left: 5%;">
+                <div class="gt-overlay-label" style="top: 75.5%; left: 5%;">
                     Навантаження<br>
                     житлового будинку<br>
                     «{{field13}} кВт»
                 </div>
                 
                 <!-- Інвертор -->
-                <div class="gt-overlay-label" style="top: 68%; left: 66%;">
+                <div class="gt-overlay-label" style="top: 69%; left: 66%;">
                     Гібридний інвертор:<br>
                     Модель: «{{field27}}»<br>
                     Верхній рівень напруги: 253 В<br>
@@ -819,7 +818,7 @@ const GT_TEMPLATES = {
                 </div>
                 
                 <!-- Панелі -->
-                <div class="gt-overlay-label" style="top: 85%; left: 68%;">
+                <div class="gt-overlay-label" style="top: 86.5%; left: 68%;">
                     Номінальна потужність:<br>
                     «{{field22}} кВт»<br>
                     Модель:<br>
